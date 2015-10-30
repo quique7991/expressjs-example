877a2763fe4912a8dd155bd09173b81d7dee2db8
Enrique Saurez <quique7991@gmail.com>
2015-10-30 10:09:51 -0400

    Complete Homework

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ app.use(bodyParser.json());
 
 // Include the Todo-lists service and let it bind to the application
 var todoService = require('./services/todo.js');
+var userService = require('./services/user.js');
 todoService.LinkApp(app);
+userService.LinkApp(app);
 
 var server = app.listen(8080, function() {
 	var host = server.address().address;
 	var port = server.address().port;
 
-	console.log('Direct your browser to http://%s:%s/api/todo-lists', host, port);
+	console.log('Direct your browser to http://%s:%s/api/users', host, port);
 });
